1b0f7fa545ce69c9aa292906525cb436b6327c39
Abdelrhman Emad <abdelrmanemad999@gmail.com>
2025-10-04 08:20:44 +0300

    first commit

---
+++ app/src/main/java/com/example/smart_food_planner/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.smart_food_planner.utils
+
+const val BaseUrl = "https://generativelanguage.googleapis.com/"
+const val Gemini_API_Key = "AIzaSyB3shG2JJV23Zmor2V94pHtCPBd_7ZRjQk"
+
+
