0848db93435bded7db0cc43fed85528a502d499c
Abdelrhman Emad <abdelrmanemad999@gmail.com>
2025-11-09 21:17:46 +0200

    In This Commit i make an edit in the Dialog of the add / edit Meal , the error is that there is a white part appears at the bottom of the dialog and some items are exist in the wrong place

--- app/src/main/java/com/example/smart_food_planner/ui/Calender.kt
+++ app/src/main/java/com/example/smart_food_planner/ui/Calender.kt
@@ -3,6 +3,7 @@ package com.example.smart_food_planner.ui
 import android.app.AlertDialog
 import android.app.Application
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -424,6 +425,8 @@ class Calender : Fragment() {
             dialog.dismiss()
         }
 
+        dialog.window?.setBackgroundDrawableResource(R.drawable.chat_background)
+
         editButton.setOnClickListener {
             dialog.dismiss()
             showAddEditMealDialog(mealNumber, false, today, thisMonth, thisYear)
@@ -465,6 +468,11 @@ class Calender : Fragment() {
         val dialog = AlertDialog.Builder(requireContext(), R.style.CustomDialog)
             .setView(dialogView)
             .create()
+
+        dialog.window?.setBackgroundDrawableResource(R.drawable.chat_background)
+
+
+
 
         val MealcardView = dialogView.findViewById<CardView>(R.id.cvMealCard)
         val EmptycardView = dialogView.findViewById<CardView>(R.id.cvEmptyMealCard)
